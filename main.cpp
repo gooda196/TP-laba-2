0dffe28f298a3c0ba39c4eb6bee3814b386794e8
Nikita <goolda196@mail.ru>
2015-09-18 10:36:42 +0400

    1.5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "sdt.h"
 int main()
 {
-    double a,b;
+    double a,b,sum;
     cout <<"Vvedite chislo a:" ;
     cin >> a;
     cout <<"Vvedite chislo b:" ;
     cin >>b;
+   sum=a+b;
+   cout << "Summa = "<<sum<<".\n";
 }
